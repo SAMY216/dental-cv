b891d6f48bb8ed426ce6fb0d3da58a77ee426028
Samy Ahmed <samy.seleman45@gmail.com>
2025-04-03 23:51:54 +0200

    4-3-2025/11-51

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,8 @@ export default function LandingPage() {
   return (
     <div className="mx-auto flex flex-col lg:flex-row items-center justify-center h-screen bg-gray-900 lg:gap-x-24 pt-10">
       <div className="text-center text-gray-100 px-10 lg:w-1/2">
-        <h1 className="text-3xl md:text-4xl font-bold">
-          I'm <span className="text-yellow-500">Samy Ahmed</span>
+        <h1 className="text-3xl md:text-4xl font-bold text-yellow-500">
+          Samy Ahmed
         </h1>
         <h2 className="text-xl md:text-2xl font-bold mt-5">
           I'm a dental student in my last year
