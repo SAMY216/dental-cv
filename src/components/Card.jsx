08f7d213a0ca6fcc5bf4900104ada90e273fe134
Samy Ahmed <samy.seleman45@gmail.com>
2025-04-03 06:42:53 +0200

    fixing images issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import image1 from "../data/cr-2-1.jpg";
+
 export default function Card({ caseData }) {
   const [isOpen, setIsOpen] = useState(false);
   const modalRef = useRef(null);
@@ -24,7 +24,7 @@ export default function Card({ caseData }) {
           {imagesToShow.map((imgSrc, j) => (
             <div key={j} className="relative">
               <img
-                src={image1}
+                src={import.meta.env.BASE_URL + imgSrc}
                 className="w-full h-auto object-cover rounded-md"
                 alt={`Case - Image ${j + 1}`}
               />
